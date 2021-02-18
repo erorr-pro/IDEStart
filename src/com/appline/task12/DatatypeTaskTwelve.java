498f6d4410d714414337d761b6e5624373a60fde
erorr-pro <my.dalar@gmail.com>
2021-02-19 02:13:37 +0300

    готов task 12

---
+++ src/com/appline/task12/DatatypeTaskTwelve.java
@@ -0,0 +1,42 @@
+package com.appline.task12;
+
+import sun.awt.HKSCS;
+
+import java.util.Scanner;
+
+public class DatatypeTaskTwelve {
+    // static Scanner scanner = new Scanner(System.in);
+    static String stopWord = "I like Java!!!";
+
+    public static void main(String[] args) {
+
+        boolean isJavaWordContains = stopWord.contains("Java");
+        if (isJavaWordContains){
+            System.out.println("Строка содержит 'Java'");
+        } else {
+            System.out.println("Строка НЕ содержит 'Java'");
+        }
+
+        boolean isILikeWordsStartsWith = stopWord.startsWith("I like");
+        if (isILikeWordsStartsWith){
+            System.out.println("Строка начинается с 'I like'");
+        } else {
+            System.out.println("Строка НЕ начинается с 'I like'");
+        }
+
+        boolean isWordEndWith = stopWord.endsWith("!!!");
+        if (isWordEndWith){
+            System.out.println("Строка заканчивается на '!!!'");
+        } else {
+            System.out.println("Строка НЕ заканчивается на '!!!'");
+        }
+
+        if (isJavaWordContains & isILikeWordsStartsWith & isWordEndWith) {
+            System.out.println(stopWord.toUpperCase()); // выводим строку если 3 условия истины
+        }
+
+        // Замена всех символов ‘a’ на ‘о’ и вывод подстроки “Jovo”
+        System.out.println(stopWord.replace("a","o").substring(7,11));
+
+    }
+}
