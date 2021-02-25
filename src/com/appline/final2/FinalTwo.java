9bf7d5633b2fae4b2174c8ecca8ff6694bf79dcb
erorr-pro <my.dalar@gmail.com>
2021-02-25 21:32:45 +0300

    финальная задача 2. Черновик

---
+++ src/com/appline/final2/FinalTwo.java
@@ -0,0 +1,22 @@
+package com.appline.final2;
+
+import java.util.Scanner;
+
+class FinalTwo {
+    static Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        //double x = scanner.nextDouble();
+
+        System.out.println("Программа решает уравнение вида 'x+a=b': ");
+        System.out.print("Введите число 'a': ");
+        double a = scanner.nextDouble();
+        System.out.print("Введите число 'b': ");
+        double b = scanner.nextDouble();
+        double x = -b + a;
+        System.out.println("Решение уравнения x + " + a + " = " + b + ";");
+        System.out.println("x = " + x);
+    }
+}
+
