7f47ba486edcb944665e7e933c6d742a5396fdf5
erorr-pro <my.dalar@gmail.com>
2021-02-28 15:47:36 +0300

    финальная задача 4. готова

---
+++ src/com/appline/final4/FinalFour.java
@@ -0,0 +1,39 @@
+package com.appline.final4;
+
+import java.util.Scanner;
+
+public class FinalFour {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает. Кто это?");
+        test();
+    }
+
+    private static void test() {
+        int tryNum = 0;
+        boolean isNotPromptUsed = true;
+        while (tryNum < 3) {
+            System.out.println("Введите ответ " + (tryNum + 1) + ": ");
+            String answer = scanner.nextLine();
+
+            if ("Подсказка".equalsIgnoreCase(answer)) {
+                if (isNotPromptUsed) {
+                    System.out.println("Какая-то подсказка");
+                    isNotPromptUsed = false;
+                } else {
+                    System.out.println("Подсказка не доступна");
+                }
+                continue;
+            }
+            if (answer.equals("Заархивированный вирус")) {
+                System.out.println("Правильно!");
+                break;
+            }
+            tryNum++;
+        }
+        if (tryNum == 3) {
+            System.out.println("Обидно, приходи в другой раз!");
+        }
+    }
+}
