18577b27e32f2a3e84e23f5333b64a0a84521e8a
erorr-pro <my.dalar@gmail.com>
2021-03-01 02:09:48 +0300

    финальная задача 2. решение через коленку n

--- src/com/appline/final2/FinalTwo.java
+++ src/com/appline/final2/FinalTwo.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 class FinalTwo {
     static Scanner scanner = new Scanner(System.in);
 
-
     public static void main(String[] args) {
         String str = takeStr();
         String operation = getOperation(str);
@@ -27,13 +26,11 @@ class FinalTwo {
         return operation;
     }
 
-
     private static void calc(String str, String operation) {
 
         int firstNumber = Character.digit(str.charAt(0), 10);
         int secondNumber = Character.digit(str.charAt(2), 10);
         int thirdNumber = Character.digit(str.charAt(4), 10);
-
 
         if (str.charAt(0) == 'x' & operation.equals("-")) { //x-2=8
             System.out.println("x=" + (thirdNumber + secondNumber));
@@ -53,33 +50,6 @@ class FinalTwo {
         if (str.charAt(4) == 'x' & operation.equals("+")) { //2+8=x
             System.out.println("x=" + (firstNumber + secondNumber));
         }
-
     }
 }
 
-
-//if (str.charAt(0) == 'x' & operation.equals("-")) { //x+2=8 //x-2=8
-//        System.out.println("x=" + ((Character.digit(str.charAt(4), 10)) + operation + (Character.digit(str.charAt(2), 10))));
-//        }
-//
-//        if (str.charAt(2) == 'x') { //2+x=8 ////2-x=8
-//        System.out.println("x=" + ((Character.digit(str.charAt(4), 10)) - (Character.digit(str.charAt(0), 10))));
-//        }
-//
-//        if (str.charAt(4) == 'x') { //2+8=x //2-8=x
-//        System.out.println("x=" + ((-1)*((Character.digit(str.charAt(0), 10)) + (Character.digit(str.charAt(2), 10)))));
-//
-//        }
-
-
-//            System.out.println(Character.digit(equation.charAt(0), 10));
-//            //System.out.println(Character.digit(equation.charAt(1), 10));
-//            System.out.println(Character.digit(equation.charAt(2), 10));
-//            //System.out.println(Character.digit(equation.charAt(3), 10));
-//            System.out.println(Character.digit(equation.charAt(4), 10));
-//
-//            System.out.println(equation.charAt(0));
-//            //System.out.println(equation.charAt(1));
-//            System.out.println(equation.charAt(2));
-//            //System.out.println(equation.charAt(3));
-//            System.out.println(equation.charAt(4));
